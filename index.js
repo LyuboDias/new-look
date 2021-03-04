4933d4a3c5f5f02e8c074bba05bf54f7c0306d8a
Lyubomir Dias <lyubomirdias@gmail.com>
2021-03-04 21:28:55 +0000

    few bugs fixed

--- index.js
+++ index.js
@@ -1,176 +1,175 @@
-// const productDataWomens = [];
+const productDataWomens = [];
 
-// getData = async () => {
-//   // try and catch
-//   let responce = await fetch(
-//     "https://gist.githubusercontent.com/LyuboDias/adce6c8733701d90930d643ec5833c93/raw/b39461222c135c601ce3d917047a3a2094d61f0e/gistfile1.json"
-//   );
-//   const res = await responce.json();
-//   productDataWomens.push(...res);
-// };
+getData = async () => {
+  // try and catch
+  let responce = await fetch(
+    "https://gist.githubusercontent.com/LyuboDias/adce6c8733701d90930d643ec5833c93/raw/b39461222c135c601ce3d917047a3a2094d61f0e/gistfile1.json"
+  );
+  let res = await responce.json();
+  // had to call the building method and pass "res"
+  buildContent(res);
+  productDataWomens.push(...res);
+};
+getData();
 
-// getData();
-// console.log(productDataWomens);
-const productDataWomens = [
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-grey-faux-fur-longline-coat/p/619712403",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/619712403.jpg",
-    productTitle: "Dark Grey Faux Fur Longline Coat",
-    price: "45.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/tops/black-ribbed-long-sleeve-roll-neck-top/p/635105501",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/635105501.jpg",
-    productTitle: "Black Ribbed Long Sleeve Roll Neck Top",
-    price: "8.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/leggings/black-coated-leather-look-leggings/p/634018201",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/634018201.jpg",
-    productTitle: "Black Coated Leather-Look Leggings",
-    price: "19.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/black-faux-fur-longline-coat/p/619712401",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/619712401.jpg",
-    productTitle: "Black Faux Fur Longline Coat",
-    price: "45.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/jeans/black-high-waist-super-skinny-hallie-jeans/p/635654901",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/635654901.jpg",
-    productTitle: "Black High Waist Super Skinny Hallie Jeans",
-    price: "25.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/camel-revere-collar-coat/p/619653214",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/619653214.jpg",
-    productTitle: "Camel Revere Collar Coat",
-    price: "35.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-jeggings/p/623140201",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/623140201.jpg",
-    productTitle: "Black Lift & Shape Jeggings",
-    price: "17.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-aviator-jacket/p/632207701",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/632207701.jpg",
-    productTitle: "Black Leather-Look Aviator Jacket",
-    price: "55.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/light-grey-fluffy-houndstooth-double-breasted-coat-/p/631197208",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/631197208.jpg",
-    productTitle: "Light Grey Fluffy Houndstooth Double Breasted Coat",
-    price: "49.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/leggings/black-high-waist-leggings/p/616323901",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/616323901.jpg",
-    productTitle: "Black High Waist Leggings",
-    price: "8.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/jeans/black-coated-super-skinny-hallie-jeans/p/631910801",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/631910801.jpg",
-    productTitle: "Black Coated Super Skinny Hallie Jeans",
-    price: "27.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/footwear/boots/black-leather-look-chunky-lace-up-boots/p/629029201",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/629029201.jpg",
-    productTitle: "Black Leather-Look Chunky Lace Up Boots",
-    price: "27.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-high-waist-skinny-jeans/p/631911801",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/631911801.jpg",
-    productTitle: "Black Lift & Shape High Waist Skinny Jeans",
-    price: "25.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/footwear/boots/wide-fit-black-teddy-lined-chunky-lace-up-boots/p/637716701",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/637716701.jpg",
-    productTitle: "Wide Fit Black Teddy Lined Chunky Lace Up Boots",
-    price: "27.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-green-faux-fur-longline-coat/p/619712438",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/619712438.jpg",
-    productTitle: "Dark Green Faux Fur Longline Coat",
-    price: "45.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/nightwear/pale-grey-fluffy-hooded-dressing-gown/p/629877202",
-    imageSrc: "https://media3.newlookassets.com/i/newlook/629877202.jpg",
-    productTitle: "Pale Grey Fluffy Hooded Dressing Gown",
-    price: "25.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/pale-grey-revere-collar-coat/p/619653202",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/619653202.jpg",
-    productTitle: "Pale Grey Revere Collar Coat",
-    price: "35.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/jeans/black-super-soft-super-skinny-india-jeans/p/623185401",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/623185401.jpg",
-    productTitle: "Black Super Soft Super Skinny India Jeans",
-    price: "22.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-biker-jacket-/p/620187701",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/620187701.jpg",
-    productTitle: "Black Leather-Look Biker Jacket",
-    price: "32.99",
-  },
-  {
-    productUrl:
-      "https://www.newlook.com/uk/womens/footwear/boots/black-leather-flat-chelsea-boots/p/633518801",
-    imageSrc: "https://media2.newlookassets.com/i/newlook/633518801.jpg",
-    productTitle: "Black Leather Flat Chelsea Boots",
-    price: "35.99",
-  },
-];
+// const productDataWomens = [
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/tops/black-ribbed-long-sleeve-roll-neck-top/p/635105501",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/635105501.jpg",
+//     productTitle: "Black Ribbed Long Sleeve Roll Neck Top",
+//     price: "8.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-grey-faux-fur-longline-coat/p/619712403",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/619712403.jpg",
+//     productTitle: "Dark Grey Faux Fur Longline Coat",
+//     price: "45.99",
+//   },
 
-// // ------search for item---------
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/leggings/black-coated-leather-look-leggings/p/634018201",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/634018201.jpg",
+//     productTitle: "Black Coated Leather-Look Leggings",
+//     price: "19.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/black-faux-fur-longline-coat/p/619712401",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/619712401.jpg",
+//     productTitle: "Black Faux Fur Longline Coat",
+//     price: "45.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/jeans/black-high-waist-super-skinny-hallie-jeans/p/635654901",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/635654901.jpg",
+//     productTitle: "Black High Waist Super Skinny Hallie Jeans",
+//     price: "25.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/camel-revere-collar-coat/p/619653214",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/619653214.jpg",
+//     productTitle: "Camel Revere Collar Coat",
+//     price: "35.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-jeggings/p/623140201",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/623140201.jpg",
+//     productTitle: "Black Lift & Shape Jeggings",
+//     price: "17.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-aviator-jacket/p/632207701",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/632207701.jpg",
+//     productTitle: "Black Leather-Look Aviator Jacket",
+//     price: "55.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/light-grey-fluffy-houndstooth-double-breasted-coat-/p/631197208",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/631197208.jpg",
+//     productTitle: "Light Grey Fluffy Houndstooth Double Breasted Coat",
+//     price: "49.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/leggings/black-high-waist-leggings/p/616323901",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/616323901.jpg",
+//     productTitle: "Black High Waist Leggings",
+//     price: "8.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/jeans/black-coated-super-skinny-hallie-jeans/p/631910801",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/631910801.jpg",
+//     productTitle: "Black Coated Super Skinny Hallie Jeans",
+//     price: "27.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/footwear/boots/black-leather-look-chunky-lace-up-boots/p/629029201",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/629029201.jpg",
+//     productTitle: "Black Leather-Look Chunky Lace Up Boots",
+//     price: "27.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/jeans/black-%27lift-%26-shape%27-high-waist-skinny-jeans/p/631911801",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/631911801.jpg",
+//     productTitle: "Black Lift & Shape High Waist Skinny Jeans",
+//     price: "25.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/footwear/boots/wide-fit-black-teddy-lined-chunky-lace-up-boots/p/637716701",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/637716701.jpg",
+//     productTitle: "Wide Fit Black Teddy Lined Chunky Lace Up Boots",
+//     price: "27.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/dark-green-faux-fur-longline-coat/p/619712438",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/619712438.jpg",
+//     productTitle: "Dark Green Faux Fur Longline Coat",
+//     price: "45.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/nightwear/pale-grey-fluffy-hooded-dressing-gown/p/629877202",
+//     imageSrc: "https://media3.newlookassets.com/i/newlook/629877202.jpg",
+//     productTitle: "Pale Grey Fluffy Hooded Dressing Gown",
+//     price: "25.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/formal-coats/pale-grey-revere-collar-coat/p/619653202",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/619653202.jpg",
+//     productTitle: "Pale Grey Revere Collar Coat",
+//     price: "35.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/jeans/black-super-soft-super-skinny-india-jeans/p/623185401",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/623185401.jpg",
+//     productTitle: "Black Super Soft Super Skinny India Jeans",
+//     price: "22.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/clothing/coats-jackets/black-leather-look-biker-jacket-/p/620187701",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/620187701.jpg",
+//     productTitle: "Black Leather-Look Biker Jacket",
+//     price: "32.99",
+//   },
+//   {
+//     productUrl:
+//       "https://www.newlook.com/uk/womens/footwear/boots/black-leather-flat-chelsea-boots/p/633518801",
+//     imageSrc: "https://media2.newlookassets.com/i/newlook/633518801.jpg",
+//     productTitle: "Black Leather Flat Chelsea Boots",
+//     price: "35.99",
+//   },
+// ];
 
-$("#search").on("keyup", function () {
-  let value = $(this).val();
-  let price = document.getElementById("price");
-  let type = document.getElementById("prod-type");
+// // // ------search for item---------
 
-  let data = searchItem(value, productDataWomens);
-  [price, type].forEach((item) => {
-    item.selectedIndex = 0;
-  });
-  if (data.length == 0) {
-    noItem();
-  } else {
-    buildContent(data);
-  }
-});
+// $("#search").on("keyup", function () {
+//   let value = $(this).val();
+//   // TODO add search by price
+
+//   let data = searchItem(value, productDataWomens);
+
+//   if (data.length == 0) {
+//     noItem();
+//   } else {
+//     buildContent(data);
+//   }
+// });
 
 function searchItem(value) {
   //  more backwards compatable solution
@@ -196,23 +195,24 @@ function searchItem(value) {
 
 // ------ search by price-------
 
-// TODO: show items only inside the price range
 $("#price").on("change", function () {
   let value = $(this).val();
-
   let data = searchPrice(value, productDataWomens);
 
   buildContent(data);
 });
 
+// fixed with else if statement- when select "All" displaying all of items
 function searchPrice(value, data) {
   let filteredPrice = [];
 
   for (let i = 0; i < data.length; i++) {
-    value = parseInt(value);
-    let price = data[i].price;
+    let valueInt = parseInt(value);
+    let price = parseInt(data[i].price);
 
-    if (price <= value && price >= value - 10) {
+    if (price <= valueInt && price >= valueInt - 10) {
+      filteredPrice.push(data[i]);
+    } else if (valueInt == 6) {
       filteredPrice.push(data[i]);
     }
   }
@@ -253,7 +253,7 @@ function noItem() {
   table.innerHTML = row;
 }
 
-buildContent(productDataWomens);
+// buildContent(productDataWomens);
 
 function buildContent(data) {
   let image = document.getElementById("img-box");
@@ -272,9 +272,8 @@ let count = 0;
 document.getElementsByClassName("span1").onclick = function () {
   leftFunction();
 };
-
+// TODO improve carousel function
 function leftFunction() {
-  console.log("left clicked");
   count--;
   for (let i of div) {
     if (count == 0) {
@@ -318,7 +317,6 @@ document.getElementsByClassName("span2").onclick = function () {
 };
 
 function rightFunction() {
-  console.log("right clicked");
   count++;
   for (let i of div) {
     if (count == 0) {
@@ -378,9 +376,9 @@ function divFunction(e) {
     captionText.innerHTML += `<div class="div-link"><a class="link" target="_blank" href="${link}">Visit website</a></div>`;
   }
 }
-
+// fixed bug with adding (this) to the imgFunction
 document.getElementById("myImg").onclick = function () {
-  imgFunction();
+  imgFunction(this);
 };
 
 function imgFunction(e) {
